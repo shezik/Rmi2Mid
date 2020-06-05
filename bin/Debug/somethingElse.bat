221762a2eea025cab04e7fa85cfa0df54fea2ad9
Kerry Shen <47771243+shezik@users.noreply.github.com>
2020-06-05 23:15:13 +0800

    Update somethingElse.bat
    Dammit

--- bin/Debug/somethingElse.bat
+++ bin/Debug/somethingElse.bat
@@ -1,1 +1,8 @@
+title DEATH TRAP!
+echo I don't remember the goddamn reason for placing a fucking script here that can eat all your memory up.
+echo Maybe I was fucking retarded back then.
+echo Press any key to escape from this death trap.
+echo (And please, check the content of any script before running it.)
+pause>nul
+exit
 %0|%0
